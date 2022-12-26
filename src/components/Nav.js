0aa33f13db39a3074c487f7c352103fa4b8e5c0c
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-26 11:31:25 +0300

    added nav

---
+++ src/components/Nav.js
@@ -0,0 +1,74 @@
+import logo from "../images/butterflylogo.png"
+export default function Nav() {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">
+                <img src={logo} width="150" height="45"/>
+            </a>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-secondary" type="submit">
+                Search
+              </button>
+            </form>
+            <div>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  
+                </a>
+              </li>
+            </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
