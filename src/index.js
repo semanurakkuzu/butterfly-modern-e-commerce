fa1ee7f11c802caa8156f24e7d98bffd852c51fd
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-27 16:12:58 +0300

    added redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import About from "./components/About"
