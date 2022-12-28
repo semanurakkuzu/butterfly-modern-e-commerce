fd9b98896bcac7fd2a4ca5e4c32b8b39d1cba497
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-28 13:43:13 +0300

    added SetImages

--- src/redux/images.js
+++ src/redux/images.js
@@ -1,25 +1,18 @@
-import { createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   data: [],
-}
+};
 export const imagesSlice = createSlice({
-  name: 'images',
+  name: "images",
   initialState,
   reducers: {
-    getToImg: (state, action) => {
+    setImages: (state, action) => {
       state.data = action.payload;
     },
-    
-    getToFilterImg:(state,action) => {
-        state.data = state.data.filter((img) => {
-            return img.category = action.payload;
-          });
-    }
   },
-})
+});
 
-// Action creators are generated for each case reducer function
-export const { getToImg, getToFilterImg } = imagesSlice.actions
+export const { setImages } = imagesSlice.actions;
 
-export default imagesSlice.reducer
+export default imagesSlice.reducer;
