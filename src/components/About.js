df4525552fd8c9aa4158ce45b56eff5d39b4b804
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-28 16:57:40 +0300

    added about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,25 @@
+import image from "../images/about_image.png";
+import Nav from "./Nav";
+
 export default function About() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <>
+      <Nav></Nav>
+      <div className="container mt-170">
+        <div className="row text-center d-flex align-items-center">
+          <div className="col">
+            <p className="fs-1 fw-bold"> Photos for everyone</p>
+            <p className="fs-6">
+              Over 3 million free high-resolution images brought to you by the
+              world’s most generous community of photographers.
+            </p>
+            <button className="btn btn-secondary">Start Browsing</button>
+          </div>
+          <div className="col">
+            <img src={image}></img>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
